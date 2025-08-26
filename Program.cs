f0f7a7fed1da6bf3076ba83af44d06d007f7998c
Bárbara Azevedo <barbara.azevedo22@gmail.com>
2025-08-25 22:46:44 -0300

    desafio 01

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Desafio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write($"Digite o primeiro valor:");
+            int valueOne = int.Parse(Console.ReadLine());
+            Console.Write($"Digite o segundo valor:");
+            int valueTwo = int.Parse(Console.ReadLine());
+            Console.Write($"Digite o terceiro valor:");
+            int valueThree = int.Parse(Console.ReadLine());
+
+            Console.Write($"Valores digitados: {valueOne}, {valueTwo}, {valueThree}");
+
+            int temp = valueTwo;
+            valueTwo = valueThree;
+            valueThree = valueOne;
+            valueOne = temp;
+
+            Console.Write($"\nValores após a troca: {valueOne}, {valueTwo}, {valueThree}");
+        }
+    }
+}
