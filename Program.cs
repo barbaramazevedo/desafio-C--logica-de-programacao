654f539d20be69d35f94c052997135b09940e346
Bárbara Azevedo <barbara.azevedo22@gmail.com>
2025-08-26 16:11:22 -0300

    desafio 10

--- Program.cs
+++ Program.cs
@@ -4,11 +4,41 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Digite um número para verificar a tabuada:");
-            int numero = Convert.ToInt32(Console.ReadLine());
-            for (int i = 1; i <= 10; i++)
+            Console.WriteLine("Digite um valor para efetuar o saque:");
+            int valor = int.Parse(Console.ReadLine());
+
+            var resultado = Sacar(valor);
+
+            Console.WriteLine("\nCédulas entregues:");
+            foreach (var item in resultado)
             {
-                Console.Write($"{numero} x {i} = {numero * i}\n");
+                Console.WriteLine($"{item.Value} cédula(s) de R$ {item.Key}");
+            }
+
+
+
+            static Dictionary<int, int> Sacar(int valor)
+            {
+                int[] cedulas = { 200, 100, 50, 20, 10, 5, 2 };
+                Dictionary<int, int> relacaoCedulas = new Dictionary<int, int>();
+
+                int restante = valor;
+
+                foreach (int nota in cedulas)
+                {
+                    if (restante >= nota)
+                    {
+                        int quantidade = restante / nota;
+                        relacaoCedulas[nota] = quantidade;
+                        restante %= nota;
+                    }
+                }
+
+                if (restante != 0)
+                {
+                    Console.WriteLine("Não é possível sacar o valor exato com as cédulas disponíveis. Valor restante: R$ " + restante);
+                }
+                return relacaoCedulas;
             }
         }
     }
