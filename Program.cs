f2cf84178cb68ece7d247136164076ff228f544a
Bárbara Azevedo <barbara.azevedo22@gmail.com>
2025-08-26 15:10:25 -0300

    desafio 08

--- Program.cs
+++ Program.cs
@@ -4,16 +4,23 @@
     {
         static void Main(string[] args)
         {
-            List<int> numeros = new List<int>();
-            Console.Write($"Digite o primeiro valor:");
-            numeros.Add(Convert.ToInt32(Console.ReadLine()));
-            Console.Write($"Digite o segundo valor:");
-            numeros.Add(Convert.ToInt32(Console.ReadLine()));
-            Console.Write($"Digite o tercero valor:");
-            numeros.Add(Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine("Números de 0 a 25 (com incremento de 1):");
+            for (int i = 0; i <= 25; i++)
+            {
+                if (i < 25)
+                    Console.Write(i + ", ");
+                else
+                    Console.Write(i);
+            }
 
-            Console.Write($"Valores em ordem crescente:");
-            Console.WriteLine(string.Join(", ", numeros.OrderBy(n => n)));
+            Console.WriteLine("\nNúmeros de 0 a 200 (com incremento de 10):");
+            for (var i = 10; i <= 200; i += 10)
+            {
+                 if (i < 200)
+                    Console.Write(i + ", ");
+                else
+                    Console.Write(i);
+            }
         }
     }
 }
