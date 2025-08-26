4b470e86db10c730ebb74d71e09b929e8124a519
Bárbara Azevedo <barbara.azevedo22@gmail.com>
2025-08-26 15:00:25 -0300

    desafio 7

--- Program.cs
+++ Program.cs
@@ -1,52 +1,19 @@
-﻿﻿namespace Desafio1
+﻿namespace Desafio1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            List<int> numeros = new List<int>();
             Console.Write($"Digite o primeiro valor:");
-            decimal valueOne = Convert.ToDecimal(Console.ReadLine());
+            numeros.Add(Convert.ToInt32(Console.ReadLine()));
             Console.Write($"Digite o segundo valor:");
-            decimal valueTwo = Convert.ToDecimal(Console.ReadLine());
+            numeros.Add(Convert.ToInt32(Console.ReadLine()));
+            Console.Write($"Digite o tercero valor:");
+            numeros.Add(Convert.ToInt32(Console.ReadLine()));
 
-            while (valueOne == valueTwo)
-            {
-                Console.Write("Os valores são iguais, digite um valor diferente para o segundo valor: ");
-                valueTwo = Convert.ToDecimal(Console.ReadLine());
-            }
-
-            decimal soma = valueOne + valueTwo;
-            decimal subtracao = valueOne - valueTwo;
-            decimal multiplicacao = valueOne * valueTwo;
-
-            Console.WriteLine($"Digite o tipo de operação que deseja realizar: \n 1 - Soma \n 2 - Subtração \n 3 - Multiplicação \n 4 - Divisão");
-            int operacao = Convert.ToInt32(Console.ReadLine());
-            switch (operacao)
-            {
-                case 1:
-                    Console.WriteLine($"Valor da soma dos dois valores: {soma}");
-                    break;
-                case 2:
-                    Console.WriteLine($"Valor da subtração dos dois valores: {subtracao}");
-                    break;
-                case 3:
-                    Console.WriteLine($"Valor da multiplicação dos dois valores: {multiplicacao}");
-                    break;
-                case 4:
-                    if (valueTwo == 0)
-                    {
-                        Console.WriteLine("Não é possível realizar a divisão por zero.");
-                    }
-                    else
-                    {
-                        decimal divisao = valueOne / valueTwo;
-                        Console.WriteLine($"Valor da divisão dos dois valores: {divisao}");
-                    }
-                    break;
-                default:
-                    Console.WriteLine("Operação inválida.");
-                    break;
-            }
+            Console.Write($"Valores em ordem crescente:");
+            Console.WriteLine(string.Join(", ", numeros.OrderBy(n => n)));
         }
     }
 }
--- obj/Debug/net8.0/Desafio1.AssemblyInfo.cs
+++ obj/Debug/net8.0/Desafio1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Desafio1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d0b58059a7dacc4b7fa3a77ca42ec4bbaf412a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce05abb98f45eeb065f5f728efde51e27d9170b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Desafio1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Desafio1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
